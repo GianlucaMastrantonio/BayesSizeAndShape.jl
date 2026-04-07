4ff7f271e6de86c0456b48e3c21e019cd924837f
Justgimmi <sangiovanni.1889445@studenti.uniroma1.it>
2026-04-07 11:30:33 +0200

    Implement a new function to center the categorical variables in automatic way

--- src/general_functions.jl
+++ src/general_functions.jl
@@ -186,38 +186,107 @@ function compute_sizeshape_fromnolocdata(landmarks::Array{Float64,3}, keepreflec
 end
 
 
-function create_designmatrix(fm::FormulaTerm, covariates::DataFrame, k::Int64 )::Tuple{Array{Float64, 3}, Int64, Vector{String}, Matrix{Float64}}
+# function create_designmatrix(fm::FormulaTerm, covariates::DataFrame, k::Int64 )::Tuple{Array{Float64, 3}, Int64, Vector{String}, Matrix{Float64}}
     
 
   
 
-    #println(covariates[1:3,:])
-    #error("")
-    covariates_copy = deepcopy(covariates)
-    for i = 1:size(covariates_copy,2)
-        if  isa(covariates_copy[:,i], CategoricalArray)
+#     #println(covariates[1:3,:])
+#     #error("")
+#     covariates_copy = deepcopy(covariates)
+#     for i = 1:size(covariates_copy,2)
+#         if  isa(covariates_copy[:,i], CategoricalArray)
 
-        elseif isa(covariates_copy[1,i], Real)
-            #covariates_copy[:,i] = (covariates_copy[:,i] .- mean(covariates_copy[:,i])) ./ std(covariates_copy[:,i])
-        else
+#         elseif isa(covariates_copy[1,i], Real)
+#             #covariates_copy[:,i] = (covariates_copy[:,i] .- mean(covariates_copy[:,i])) ./ std(covariates_copy[:,i])
+#         else
         
-            error("Only factors or Real variables are allowed in covariates")
+#             error("Only factors or Real variables are allowed in covariates")
+#         end
+#     end
+
+#     designmatrix_v2_app = ModelFrame(fm, covariates_copy);
+#     designmatrix_v2 = ModelMatrix(designmatrix_v2_app).m
+#     #println(designmatrix_v2[1:3,:])
+#     #error("")
+
+
+#     designmatrix = compute_designmatrix(designmatrix_v2, k) # dimensions  k, k * d, n
+
+#     @assert sum(designmatrix_v2[:, 1] .== 1) == size(designmatrix_v2,1) "intercept needed"
+    
+#     return designmatrix, size(designmatrix_v2,2), coefnames(designmatrix_v2_app), designmatrix_v2 
+
+# end
+
+function create_designmatrix(fm::FormulaTerm, covariates::DataFrame, k::Int64)::Tuple{Array{Float64, 3}, Int64, Vector{String}, Matrix{Float64}}
+    
+    covariates_copy = deepcopy(covariates)
+    terms_in_model = StatsModels.termvars(fm)
+    n = size(covariates_copy, 1)
+    X_parts = [ones(n)]  # intercetta
+    # coef_names = ["(Intercept)"]
+    continuous_vars = [v for v in terms_in_model if !isa(covariates[!, v], CategoricalArray) && !(eltype(covariates[!, v]) <: AbstractString)]
+    for v in continuous_vars
+        push!(X_parts, Float64.(covariates_copy[!, v]))
+        # push!(coef_names, String(v))
+    end
+
+    for col_name in names(covariates_copy)
+        if Symbol(col_name) ∉ terms_in_model
+        continue
+        end
+        col_data = covariates_copy[!, col_name]
+        
+        if isa(col_data, CategoricalArray) || eltype(col_data) <: AbstractString
+            cat_col = categorical(col_data) # define the categorical valyìue 
+            covariates_copy[!, col_name] = cat_col
+            lvls = levels(cat_col) # take the levels of the categorical variables
+            l = length(lvls) # how many levels? 
+            counts = [sum(cat_col .== lvl) for lvl  in lvls] # how many units are inside each level?
+            n_l = counts[end] # the last level is the reference group level
+            n = length(cat_col)
+            Z = zeros(n, l-1)
+
+            for i in 1:n
+                g = findfirst(==(cat_col[i]), lvls)  # indice gruppo osservazione i
+                
+                for j in 1:(l-1)
+                    n_j = counts[j]
+                    
+                    if g == j
+                        Z[i, j] = 2 * n_l / (n_j + n_l)
+                    elseif g == l
+                        Z[i, j] = -2 * n_j / (n_j + n_l)
+                    else
+                        Z[i, j] = 0.0
+                    end
+                end
+            end
+            for j in 1:size(Z, 2)
+                push!(X_parts, Z[:, j])
+            end
+                        
+            #custom_contrasts[Symbol(col_name)] = HypothesisCoding(h_mat', levels=lvls)
         end
     end
 
-    designmatrix_v2_app = ModelFrame(fm, covariates_copy);
-    designmatrix_v2 = ModelMatrix(designmatrix_v2_app).m
-    #println(designmatrix_v2[1:3,:])
-    #error("")
+    # Build ModelFrame with Eq 10 contrasts
+    designmatrix_v2_app = ModelFrame(fm, covariates_copy)
+    # designmatrix_v2 = ModelMatrix(designmatrix_v2_app).m
+    designmatrix_v2 = hcat(X_parts...) # concatenate all the parts of the design matrix
+    designmatrix = compute_designmatrix(designmatrix_v2, k) 
 
-
-    designmatrix = compute_designmatrix(designmatrix_v2, k) # dimensions  k, k * d, n
-
-    @assert sum(designmatrix_v2[:, 1] .== 1) == size(designmatrix_v2,1) "intercept needed"
+    @assert all(designmatrix_v2[:, 1] .== 1.0) "Intercept needed"
     
-    return designmatrix, size(designmatrix_v2,2), coefnames(designmatrix_v2_app), designmatrix_v2 
-
+    return designmatrix, size(designmatrix_v2, 2), coefnames(designmatrix_v2_app), designmatrix_v2 
 end
+
+
+
+
+
+
 
 
 function compute_yrdata(yrdata::Array{Float64,3}, ssdata::Array{Float64,3}, rmat::Array{Float64,3})
